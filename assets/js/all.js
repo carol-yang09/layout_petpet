6186ce664587bd752bc6e65d2e97bbdddd8f9730
Carol Yang <carolyangya@gmail.com>
2021-01-02 21:15:01 +0800

    Update 2021-01-02T13:14:21.236Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,59 +1,58 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   // Navbar Toggler
-  $('.js-navbarToggler').on('click', function(e) {
+  $('.js-navbarToggler').on('click', function (e) {
     e.preventDefault();
     $('.navbar').toggleClass('navbar--active');
-  });
+  }); // Drowpdown
 
-  // Drowpdown
-  $('.js-drowpdownToggler').on('click', function(e) {
+  $('.js-drowpdownToggler').on('click', function (e) {
     e.preventDefault();
     $(this).next('.dropdown__content').slideToggle();
-  });
+  }); // open Modal
 
-  // open Modal
-  $('.js-openModal').on('click', function(e) {
+  $('.js-openModal').on('click', function (e) {
     e.preventDefault();
     $('body').attr('style', 'overflow: hidden');
     $('.modal-wrap').attr('style', 'display: flex');
     $('.step1').attr('style', 'display: flex');
   });
-
-  $('.js-toggleStep').on('click', function(e) {
+  $('.js-toggleStep').on('click', function (e) {
     e.preventDefault();
-    const action = $(this).data('action');
-    const num = $(this).data('num');
-    let stepNum = Number(num);
+    var action = $(this).data('action');
+    var num = $(this).data('num');
+    var stepNum = Number(num);
     console.log(stepNum);
+
     if (action == 'return') {
-      stepNum -= 1; 
+      stepNum -= 1;
     } else {
       stepNum += 1;
     }
-    $(`.step${stepNum}`).attr('style', 'display: flex');
-    $(this).parents('.step').attr('style', 'display: none');
-  });
 
-  // close Modal
-  $('.modal-wrap').on('click', function(e) {
-    if (e.target.className == 'modal-wrap' ) {
+    $(".step".concat(stepNum)).attr('style', 'display: flex');
+    $(this).parents('.step').attr('style', 'display: none');
+  }); // close Modal
+
+  $('.modal-wrap').on('click', function (e) {
+    if (e.target.className == 'modal-wrap') {
       $('body').attr('style', 'overflow: auto');
       $('.modal-wrap').attr('style', 'display: none');
       $('.step').attr('style', 'display: none');
     }
   });
-});
+}); // AOS
 
-// AOS
-AOS.init();
+AOS.init(); // Swiper
 
-// Swiper
-const swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
   navigation: {
     nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
-  },
-})
+    prevEl: '.swiper-button--prev'
+  }
+});
+//# sourceMappingURL=all.js.map
